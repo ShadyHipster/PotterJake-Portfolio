b6bb4ede8b14503e85a642d39432db3c02eafc52
ShadyHipster <148581282+ShadyHipster@users.noreply.github.com>
2025-07-15 19:37:01 -0400

    added theme and vite

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+    return (
+        <div className="min-h_screen">
+            {/*Theme Toggle*/}
+
+            {/*Background Effects*/}
+
+            {/*Nav Bar*/}
+
+            {/*Main Content*/}
+
+            {/*Footer*/}
+        </div>
+    );
+}
